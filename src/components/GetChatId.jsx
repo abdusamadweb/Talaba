c18eee817ec4075136779a8a9775e63af1cea1c3
abdusamadweb <abdusamadweb@gmail.com>
2025-03-29 14:31:26 +0500

    omg finish ~ 4.4

--- src/components/GetChatId.jsx
+++ src/components/GetChatId.jsx
@@ -4,27 +4,25 @@ import axios from "axios";
 const TelegramChatId = () => {
 
     const [chatId, setChatId] = useState(null);
+    const [updates, setUpdates] = useState(null);
     const [logs, setLogs] = useState('')
     const token = "7434516681:AAG5L3L5AKnNNlCBgaDzP0Y-RFO0LRfBIH8";
 
     // Твой токен
-
     useEffect(() => {
         const fetchChatId = async () => {
             try {
-                // 1️⃣ Удаляем вебхук, если он есть
                 await axios.get(`https://api.telegram.org/bot${token}/deleteWebhook`);
-
-                // 2️⃣ Получаем обновления
                 const response = await axios.get(`https://api.telegram.org/bot${token}/getUpdates`);
                 const updates = response.data.result;
 
+                setUpdates(updates); // Сохраняем все обновления
+
                 if (updates.length > 0) {
-                    const latestChatId = updates[updates.length - 1].message.chat.id;
+                    const latestChatId = updates[updates.length - 1].message?.chat?.id;
                     setChatId(latestChatId);
                     setLogs('Chat id is successfully');
                 } else {
-                    console.log("Нет сообщений, напиши боту первым!");
                     setLogs('Chat id is NOT');
                 }
             } catch (error) {
@@ -41,8 +39,9 @@ const TelegramChatId = () => {
             {chatId ? (
                 <p>Chat ID: {chatId}</p>
             ) : (
-                <p>{ logs }</p>
+                <p>{logs}</p>
             )}
+            <pre>{JSON.stringify(updates, null, 2)}</pre>
         </div>
     );
 };
